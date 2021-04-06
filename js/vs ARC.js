43475dc6349d1bf0f774251a2bc77749b9c6933a
blackkcold <blackkcold@outlook.com>
2021-04-06 19:02:24 +0800

    Update vs ARC.js

--- js/vs ARC.js
+++ js/vs ARC.js
@@ -9,34 +9,6 @@ var audio = document.getElementById("sound");
 
 var audio = document.getElementById('music1');
 
-$("#btn").bind("touchstart", function bf() {
-
-  
-
-if(audio !== null) {
-
-//检测播放是否已暂停.audio.paused 在播放器播放时返回false.
-
-//alert(audio.paused);
-
-if(audio.paused) {
-
-audio.play(); //audio.play();// 这个就是播放
-
-$("#btn").addClass("active")
-
-} else {
-
-audio.pause(); // 这个就是暂停
-
-$("#btn").removeClass("active")
-
-}
-
-}
-
-})
-
 canvas = document.getElementById('visualizer');
 canvasCtx = canvas.getContext("2d");
 //创建数据	
